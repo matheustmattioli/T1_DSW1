714d58ea3c1262ae720e4709b1304cb54793e0c5
isarbela <panzeref@gmail.com>
2022-07-29 15:31:18 -0300

    Adicionado usuario

---
+++ db/MySQL/create.sql
@@ -0,0 +1,21 @@
+drop database if exists Login;
+
+create database Login;
+
+use Login
+
+create table Client(
+id bigint not null auto_increment, 
+nome varchar(256) not null, 
+email varchar(24) not null unique, 
+cpf varchar(11) not null unique, 
+senha varchar(64) not null, 
+sexo varchar(1),
+nascimento date,
+telefone varchar(11),
+papel varchar(3) not null,
+primary key (id));
+
+insert into Usuario(nome, email, cpf, senha, nascimento, papel) values ('Administrador', 'admin@email.com', '12345678910', 'admin', 2000-01-01 ,'ADM');
+
+insert into Usuario(nome, email, cpf, senha, sexo, nascimento, telefone, papel) values ('Usuario', 'usuario@email.com', '12345678910', 'user', 'F', 2000-01-01 , '123456789' ,'USR');
---
+++ src/main/java/br/ufscar/dc/dsw/domain/Usuario.java
@@ -0,0 +1,116 @@
+package br.ufscar.dc.dsw.domain;
+
+import java.util.Date;
+
+public class Usuario {
+
+	private Long id;
+	private String nome;
+	private String email;
+	private final String cpf;
+	private String sexo;
+	private Date nascimento;
+	private String telefone;
+	private String senha;
+	private String papel;
+
+	public Usuario(Long id, String cpf) {
+		this.cpf = cpf;
+		this.id = id;
+	}
+
+	public Usuario(String nome, String email, String cpf, String sexo, Date nascimento, String telefone, String senha, String papel) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.cpf = cpf;
+		this.sexo = sexo;
+		this.nascimento = nascimento;
+		this.telefone = telefone;
+		this.senha = senha;
+		this.papel = papel;
+	}
+	
+	public Usuario(Long id, String nome, String email, String cpf, String sexo, Date nascimento, String telefone, String senha, String papel) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.cpf = cpf;
+		this.sexo = sexo;
+		this.nascimento = nascimento;
+		this.telefone = telefone;
+		this.senha = senha;
+		this.papel = papel;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String password) {
+		this.senha = password;
+	}
+
+	public String getPapel() {
+		return papel;
+	}
+
+	public void setPapel(String papel) {
+		this.papel = papel;
+	}
+
+	public Date getNascimento() {
+		return nascimento;
+	}
+
+	public void setNascimento(Date nascimento) {
+		this.nascimento = nascimento;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+}

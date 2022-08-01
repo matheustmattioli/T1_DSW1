51988ef18e505ce4bc0206f7f87823a6c86b331b
isarbela <panzeref@gmail.com>
2022-08-01 15:24:07 -0300

    Fix redirecionamento do login

--- src/main/java/br/ufscar/dc/dsw/controller/IndexController.java
+++ src/main/java/br/ufscar/dc/dsw/controller/IndexController.java
@@ -9,72 +9,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import br.ufscar.dc.dsw.dao.AgenciaDAO;
-import br.ufscar.dc.dsw.dao.UsuarioDAO;
-import br.ufscar.dc.dsw.domain.Agencia;
-import br.ufscar.dc.dsw.domain.Usuario;
-import br.ufscar.dc.dsw.util.Erro;
-
 @WebServlet(name = "Index", urlPatterns = { "/index.jsp" })
-public class IndexController extends HttpServlet {
-
-	private static final long serialVersionUID = 1L;
+public class IndexController extends HttpServlet{
+	
+private static final long serialVersionUID = 1L;
 	
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		doGet(request, response);
 	}
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		Erro erros = new Erro();
-		if (request.getParameter("bOK") != null) {
-			String email = request.getParameter("email");
-			String senha = request.getParameter("senha");
-			if (email == null || email.isEmpty()) {
-				erros.add("É necessário informar um email para logar");
-			}
-			if (senha == null || senha.isEmpty()) {
-				erros.add("É necessário informar uma senha para logar");
-			}
-			if (!erros.isExisteErros()) {
-				UsuarioDAO dao = new UsuarioDAO();
-				Usuario usuario = dao.getbyEmail(email);
-				AgenciaDAO daoAgencia = new AgenciaDAO();
-				Agencia agencia = daoAgencia.getbyEmail(email);
-				if (usuario != null) {
-					if (usuario.getSenha().equals(senha)) {
-						request.getSession().setAttribute("usuarioLogado", usuario);
-						if (usuario.getPapel().equals("ADM")) {
-							response.sendRedirect("admin/");
-						} else {
-							response.sendRedirect("usuario/");
-						}
-						return;
-					} else {
-						erros.add("Senha ou email inválidos");
-					}
-				} else if(agencia != null) {
-					if (agencia.getSenha().equals(senha)) {
-						request.getSession().setAttribute("usuarioLogado", agencia);
-						response.sendRedirect("agencia/");
-						return;
-					} else {
-						erros.add("Senha ou email inválidos");
-					}
-				} else {
-					erros.add("Usuário não encontrado");
-				}
-			}
-		}
-		request.getSession().invalidate();
 
-		request.setAttribute("mensagens", erros);
-        // request.setAttribute("pacotes", pacotes);
-
-		String URL = "/login.jsp";
+		//request.setAttribute("pacotes", pacotes);
+		
+		String URL = "/home.jsp";
 		RequestDispatcher rd = request.getRequestDispatcher(URL);
 		rd.forward(request, response);
 	}
+
 }
